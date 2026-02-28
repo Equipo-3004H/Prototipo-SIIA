89128f112620725f2f108b21dbcd635ac742fa99
Olga Sarahi Rodriguez Valladares <sarahir007@gmail.com>
2026-02-28 13:03:01 -0600

    Se agregan metodos de manejo de excepciones

--- prototipo-api/main.py
+++ prototipo-api/main.py
@@ -1,10 +1,42 @@
-from typing import Annotated
-
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, HTTPException, Request
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import PlainTextResponse, JSONResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
+# Manejadores de excepciones personalizados
+@app.exception_handler(StarletteHTTPException)
+async def http_exception_handler(request, exc):
+     return JSONResponse(
+        status_code=exc.status_code,
+        content=jsonable_encoder({"detail": exc.detail, "body": exc.body}),
+    )
 
+# Manejador de excepciones para errores de validación con detalles en formato JSON
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=422,
+        content=jsonable_encoder({"detail": exc.errors(), "body": exc.body}),
+    )
+
+# Manejador de excepciones para errores no controlados con un mensaje genérico
+@app.exception_handler(Exception)
+async def global_exception_handler(request: Request, exc: Exception):
+    return JSONResponse(
+        status_code=500,
+        content={"detail": "Error interno del servidor."},
+    )
+
+# Endpoint de ejemplo para levantar una excepción personalizada
+@app.get("/levantar-excepcion/")
+async def raise_error():
+    raise HTTPException(status_code=400, detail="Este es un error de ejemplo.")
+
+# Endpoint de ejemplo para subir un archivo y mostrar su nombre y tipo de contenido
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile):
     return {
